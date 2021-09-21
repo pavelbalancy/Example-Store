f159aeaf75adeefbc8dd5904e3cfd12b44e00e2f
Pavel Ignatov <pavel@balancy.co>
2021-09-21 23:18:21 +0300

    Added WebGL build and option to send Game_Id and Public_Key as parameters in URL

--- Assets/StoreDemo/Scripts/General/Launcher.cs
+++ Assets/StoreDemo/Scripts/General/Launcher.cs
@@ -1,20 +1,29 @@
+using System.Collections.Generic;
 using Balancy;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
+    private const string GAME_ID = "game_id";
+    private const string PUBLIC_KEY = "public_key";
+    
     [SerializeField]
     private string apiGameId;
     [SerializeField]
     private string publicKey;
 
+    private Dictionary<string, string> _urlParams;
+
     private void Start()
     {
+        _urlParams = ParseUrl();
+        
         Main.Init(new AppConfig
         {
-            ApiGameId = apiGameId,
-            PublicKey = publicKey,
+            ApiGameId = GetGameId(),
+            PublicKey = GetPublicKey(),
             Environment = Constants.Environment.Development,
+            Platform = Constants.Platform.AndroidGooglePlay,
             OnReadyCallback = response =>
             {
                 Debug.Log("Balancy Init  " + response.Success);
@@ -24,5 +33,50 @@ public class Launcher : MonoBehaviour
                     Controller.PrintAllErrors();
             }
         });
+    }
+
+    private string GetGameId()
+    {
+        if (HasBothGameIDAndPublicKey())
+            return _urlParams[GAME_ID];
+
+        return apiGameId;
+    }
+    
+    private string GetPublicKey()
+    {
+        if (HasBothGameIDAndPublicKey())
+            return _urlParams[PUBLIC_KEY];
+
+        return publicKey;
+    }
+
+    private bool HasBothGameIDAndPublicKey()
+    {
+        return _urlParams.ContainsKey(GAME_ID) && _urlParams.ContainsKey(PUBLIC_KEY);
+    }
+
+    private Dictionary<string, string> ParseUrl()
+    {
+        var paramsDict = new Dictionary<string, string>();
+
+        var url = Application.absoluteURL;
+        var splitUrl = url.Split('?');
+        if (splitUrl.Length == 2)
+        {
+            var prms = splitUrl[1];
+            var splitParams = prms.Split('&');
+            foreach (var kvp in splitParams)
+            {
+                var splitKvp = kvp.Split('=');
+                if (splitKvp.Length == 2)
+                {
+                    if (!paramsDict.ContainsKey(splitKvp[0]))
+                        paramsDict.Add(splitKvp[0], splitKvp[1]);
+                }
+            }
+        }
+
+        return paramsDict;
     }
 }
